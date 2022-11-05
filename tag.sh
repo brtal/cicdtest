beacb4f52bdb361e56f34fc16f38f4cb077d137e
Brett Taylor <brett@realbt.com>
2022-11-05 16:24:31 -0700

    check existing tag

--- tag.sh
+++ tag.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+TARGET_REF="$1"
 
 GITHUB_ACTION=${GITHUB_ACTION:-}
 
-TAG_NAME="tag-$(date +%s)"
+# find existing tags
+TAGS_AT_REF=$(git tag --points-at $TARGET_REF 'tag-*')
+if [ "$TAGS_AT_REF" != "" ]; then
+    echo "The following tags were already found. Not applying new tag."
+    echo "$TAGS_AT_REF"
+    exit 0
+fi
 
+TAG_NAME="tag-$(date +%s)"
 echo "Tag: '$TAG_NAME'"
 
 if [ -n "$GITHUB_ACTION" ]; then
