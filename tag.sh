8b01890edc9bed0c9bd34f40015bc0e640d42c65
Brett Taylor <brett@realbt.com>
2022-11-05 13:21:00 -0700

    fix github detection

--- tag.sh
+++ tag.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 
+GITHUB_ACTION=${GITHUB_ACTION:-}
+
 TAG_NAME="tag-$(date +%s)"
 
 echo "Tag: '$TAG_NAME'"
 
-if [ ! -n $GITHUB_OUTPUT ]; then
+if [ -n "$GITHUB_ACTION" ]; then
     echo "tag_name=$TAG_NAME" >> $GITHUB_OUTPUT
 fi
